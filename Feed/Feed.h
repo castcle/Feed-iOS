a25349458d0d7950e06378b4d46a334355330623
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-02 18:22:39 +0700

    Init Feed module

---
+++ Feed/Feed.h
@@ -0,0 +1,18 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by Tanakorn Phoochaliaw on 2/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
