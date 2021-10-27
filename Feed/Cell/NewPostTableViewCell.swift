1b34a5f3319fd1a3550dbb2346d8af8ae48b9cd2
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-10-27 15:43:43 +0700

    New page local model

--- Feed/Cell/NewPostTableViewCell.swift
+++ Feed/Cell/NewPostTableViewCell.swift
@@ -61,6 +61,6 @@ class NewPostTableViewCell: UITableViewCell {
     }
     
     @IBAction func profileAction(_ sender: Any) {
-        Utility.currentViewController().navigationController?.pushViewController(ProfileOpener.open(.userDetail(UserDetailViewModel(profileType: .me))), animated: true)
+        Utility.currentViewController().navigationController?.pushViewController(ProfileOpener.open(.userDetail(UserDetailViewModel(profileType: .me, page: nil))), animated: true)
     }
 }
