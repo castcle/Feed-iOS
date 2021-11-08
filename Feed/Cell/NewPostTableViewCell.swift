263b79fbc99caf0fec9aa803b0f537f01ce6400e
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-11-08 14:24:46 +0700

    Get user avatar from local

--- Feed/Cell/NewPostTableViewCell.swift
+++ Feed/Cell/NewPostTableViewCell.swift
@@ -50,8 +50,7 @@ class NewPostTableViewCell: UITableViewCell {
     }
     
     func configCell() {
-        let url = URL(string: UserManager.shared.avatar)
-        self.profileImage.kf.setImage(with: url, placeholder: UIImage.Asset.userPlaceholder, options: [.transition(.fade(0.35))])
+        self.profileImage.image = UserManager.shared.avatar
     }
     
     @IBAction func postAction(_ sender: Any) {
